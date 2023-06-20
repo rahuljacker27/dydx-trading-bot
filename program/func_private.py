dbbf3c9e6511c1e5f91c6a418e4aa1efcefde9db
rahuljacker27 <rkkumar421@gmail.com>
2023-06-20 23:16:15 +0530

    Stage 2 start

---
+++ program/func_private.py
@@ -0,0 +1,100 @@
+from datetime import datetime, timedelta
+import time
+from pprint import pprint
+from func_utils import format_number
+# Place market order
+def place_market_order(client, market, side, size, price, reduce_only):
+    # Get Position Id
+    account_response = client.private.get_account()
+    position_id = account_response.data["account"]["positionId"]
+
+    # Get expiration time
+    server_time = client.public.get_time()
+    # expiration = datetime.fromisoformat(server_time.data["iso"].replace("Z", "")) + timedelta(minutes=1)
+    # expiration_timestamp = int(expiration.timestamp())
+
+    # Calculate the expiration time as 1 minute in the future from the current time
+    # expiration = datetime.now() + timedelta(minutes=1)
+
+    # # Convert the expiration time to a Unix timestamp
+    # expiration_timestamp = int(expiration.timestamp())
+    expiration = datetime.now() + timedelta(minutes=1.1)
+    expiration_timestamp = int(expiration.timestamp())
+    # Place an order
+    placed_order = client.private.create_order(
+        position_id=position_id, # required for creating the order signature
+        market=market,
+        side=side,
+        order_type="MARKET",
+        post_only=False,
+        size=size,
+        price=price,
+        limit_fee='0.015',
+        expiration_epoch_seconds = expiration_timestamp,
+        time_in_force="FOK",
+        reduce_only=reduce_only
+    )
+
+    return placed_order.data
+
+# Abort all open positions 
+def abort_all_positions(client):
+
+    # cancel all orders
+    client.private.cancel_all_orders()
+
+    #protect API
+    time.sleep(0.5)
+
+    # Get markets for reference of tick size
+    markets = client.public.get_markets().data
+    # pprint(markets)
+
+    #protect API
+    time.sleep(0.5)
+
+    #Get all open positions
+    positions = client.private.get_positions(status="OPEN")
+    all_positions = positions.data["positions"]
+
+    # pprint(all_positions)
+
+    #Handle open positions
+    close_order = []
+    if len(all_positions) > 0:
+
+        #loop through each position
+        for position in all_positions:
+
+            #Determine Market
+            market = position["market"]
+
+            #Determine Side
+            side = "BUY"
+            if position["side"] == "LONG":
+                side = "SELL"
+            
+            # print(market, side)
+            
+            #Get price
+            price = float(position["entryPrice"])
+            accept_price = price * 1.7 if side == "BUY" else price * 0.3
+            tick_size = markets["markets"][market]["tickSize"]
+            accept_price = format_number(accept_price, tick_size)
+
+            # Place order to close
+            order = place_market_order(
+                client,
+                market,
+                side,
+                position["sumOpen"],
+                accept_price,
+                True
+            )
+
+            #Apeend the results
+            close_order.append(order)
+
+            #protect API
+            time.sleep(0.5)
+        return close_order
--- program/main.py
+++ program/main.py
@@ -1,4 +1,33 @@
-
+from constants import ABORT_ALL_POSITIONS, FIND_COINTEGRATED
+from func_connections import connect_dydx
+from func_private import abort_all_positions
+from func_public import construct_market_prices
 
 if __name__ == "__main__":
-    print("Hello bot!")
+
+    #connect to client
+    try:
+        print("Connecting to client...")
+        client = connect_dydx()
+    except Exception as e:
+        print(e)
+        print("Error connecting to client : ", e)
+        exit(1)
+
+    # Abort all open positions
+    if ABORT_ALL_POSITIONS:
+        try:
+            print("CLosing all positions...")
+            close_orders = abort_all_positions(client)
+        except Exception as e:
+            print("Error closing all positions : ", e)
+            exit(1)
+
+    #Find cointegrated Pairs
+    if FIND_COINTEGRATED:
+        try:
+            print("Fetching market prices , please allow 3 mins...")
+            df_market_price = construct_market_prices(client)
+        except Exception as e:
+            print("Error contructing market prices : ", e)
+            exit(1)
